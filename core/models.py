edadeec4f304295c487a3eac8cc88e0025b8afff
Ahan <64554828+AhanR@users.noreply.github.com>
2023-04-28 12:01:29 +0530

    final changes

--- core/models.py
+++ core/models.py
@@ -4,12 +4,10 @@ from django.db import models
 class User(models.Model) :
     username = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=50)
-    # userId = models.AutoField()
 
 class Posts(models.Model):
    postContent = models.TextField()
    postAuthor = models.ForeignKey(User, on_delete=models.CASCADE)
-#    postId = models.BigAutoField()
 
 
 class Message(models.Model) :
